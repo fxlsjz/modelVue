fb2fd56bdf43c89d01ebd5c9eb24c581351b2153
冯晓亮 <fengxiaoliang@chinamobo.com>
2017-11-21 11:06:31 +0800

    elementUI的改变

--- src/components/Example.vue
+++ src/components/Example.vue
@@ -3,6 +3,17 @@
     案例
     <el-switch v-model="value2" active-color="#13ce66" inactive-color="#ff4949"></el-switch>
     
+    <el-tag>标签一</el-tag>
+		<el-tag type="success">标签二</el-tag>
+		<el-tag type="info">标签三</el-tag>
+		<el-tag type="warning">标签四</el-tag>
+		<el-tag type="danger">标签五</el-tag>
+		
+		<div class="block">
+	    <span class="demonstration">默认</span>
+	    <el-slider v-model="valueA"></el-slider>
+	  </div>
+    
   </div>
 </template>
 
@@ -12,7 +23,8 @@ export default {
   data() {
       return {
         value1: true,
-        value2: true
+        value2: true,
+        valueA: 0
       }
   }
 }
